d3fafcc715301eea281aae702ebe579ff28b67ad
Winedays <adler011014@gmail.com>
2021-02-19 18:06:22 +0800

    send a warning mail to user when signin/out may faid
    send email to warning user when get a signin/out error massage from myntu
    change config structure

--- auto_signing.py
+++ auto_signing.py
@@ -1,21 +1,89 @@
 import os
 import json
+import argparse
 import requests
 import sys
 import configparser
 import random
 import time
+import traceback
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
+# set Argument
+def setArgument() :
+    parser = argparse.ArgumentParser()
+    # position argument
+    parser.add_argument("action", type=str, choices=["signin", "signout"], help = "action of sign in or sign out.")
+    args = parser.parse_args()
+    return args
 
 # read user config
-def readCongif( file ) :
+def readCongif( file: str ) :
     with open( file , 'r' , encoding="utf-8" ) as f :
         user = f.readline().strip()
         password = f.readline().strip()
     userDict = {'user': user, 'pass': password}  # {'user': <name>, 'pass': <password>} 
     return userDict
+
+# prepare mail message & send it
+def sendErrorMessageMail( checkMessage: str, messageDict: str, mailConfig: dict ) :
+    mailDict = {'host': None, 'user': None, 'password': None, 'sender': None,
+                'subject': None, 'from': None, 'to': None, 'body': None}
+    mail_subject = "NTU Auto Singing Wraning Massage"
+    
+    # read mail config
+    mailDict['host'] = mailConfig['Host']
+    mailDict['port'] = int(mailConfig['TlsPort'])
+    mailDict['user'] = mailConfig['User']
+    mailDict['password'] = mailConfig['Password']
+    
+    # set mail message
+    mailDict['sender'] = "NTU Auto Singing"
+    mailDict['subject'] = "NTU Auto Singing Massage (%s)" % checkMessage
+    mailDict['from'] = mailConfig['User']
+    mailDict['to'] = mailConfig['User']
+    bodyText = ""
+    if 'd' in messageDict :
+        bodyText += 'NTU Auto Singing got warning or error message at %s\n\n' % str(messageDict['d'])
+    else :
+        bodyText += 'NTU Auto Singing got unknow request from MyNTU\n\n'
+    if 'msg' in messageDict :
+        bodyText += 'System Message :%s\n\n' % str(messageDict['msg'])
+    bodyText += 'Request Json :\n%s' % str(messageDict)
+    mailDict['body'] = bodyText
+    
+    # send mail
+    sendMail( mailDict )
+    return ;
+    
+# send error massage mail
+def sendMail( mailDict: dict ) :
+    mail_host = mailDict['host']
+    mail_port = int(mailDict['port'])
+    mail_user = mailDict['user']
+    mail_pass = mailDict['password']
+
+    # mail massage
+    msg = MIMEMultipart()
+    msg['From'] = mailDict['from'] # "counter programe"
+    msg['To'] = mailDict['to'] # "test"
+    msg['Subject'] = mailDict['subject']
+    body = mailDict['body']
+
+    msg.attach(MIMEText(body, 'plain'))
+    # send mail
+    server = smtplib.SMTP(mail_host, mail_port)
+    server.starttls()
+    server.login(mail_user, mail_pass)
+    text = msg.as_string()
+    server.sendmail(msg['From'], msg['To'], text)
+    server.quit()
+    print( "Error Massage Mail Sent to " + mailDict['to'] )
+    return ;
 
 # login
 def loginMyntu( session , userInfo ) :
@@ -131,15 +199,34 @@ def signOut( session ) :
     # requests.text example : [{'t': 1, 'msg': '簽退成功(r2)。', 'd': '2020-11-23 17:59:59', 'on': '2020-11-23 08:59:59', 'off': '2020-11-23 17:59:59', 'name': 'XXX(T0000)', 'ws': '08:00', 'we': '17:00', 'wb': '60'}]
     return messageDict
     
+# check is sign in/out success & return error massage if failed
+def checkSignSuccess( messageDict ) :
+    checkDict = {'success': False, 'massage': ""}
+    if 't' not in messageDict :
+        checkDict['massage'] = "Wraning: Got a unknow request."
+    elif messageDict['t'] == 1 :
+        checkDict['success'] = True
+    else :  # 't' != 1 
+        if "不需上班" in messageDict['msg'] :
+            checkDict['massage'] = "Error: Today is non-working day"
+        elif "請先簽到" in messageDict['msg'] :
+            checkDict['massage'] = "Error: Sign out failed - No sign in record"
+        elif "本日至遲須於" in messageDict['msg'] :
+            checkDict['massage'] = "Error: Sign out failed - Too late to sign out"
+        else :
+            checkDict['massage'] = "Error: Sign in/out failed"
+    return checkDict ;
+    
 if __name__ == "__main__" :
     # get argument & config
-    action = sys.argv[1]
+    args = setArgument()
+    action = args.action
     config = configparser.ConfigParser()
     config.read("config.ini")
-    if "DEFAULT" not in config :
-        raise Exception("Config Error: section \"DEFAULT\" not find.")
-    randomDelay = config["DEFAULT"]["RandomDelay"].lower()
-    maxDelayTime = float(config["DEFAULT"]["MaxDelayTime"])
+    if "TIME_DELAY" not in config :
+        raise Exception("Config Error: section \"TIME_DELAY\" not find.")
+    randomDelay = config["TIME_DELAY"]["RandomDelay"].lower()
+    maxDelayTime = float(config["TIME_DELAY"]["MaxDelayTime"])
     
     # load user info.
     userDict = readCongif( "./user.conf" )
@@ -147,18 +234,20 @@ if __name__ == "__main__" :
     # set delay time
     if randomDelay == "true" :
         delay = random.random() * maxDelayTime * 60  # secs
-        time.sleep( delay )  # delay before singin/signout
+        # time.sleep( delay )  # delay before singin/signout
     
+    messageDict = None
+    checkDict = None
     try :
         # create session
         session = requests.Session()
         session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
         session.headers['Connection'] = 'keep-alive'
-
+        
         # login myntu
         attendPages = loginMyntu( session , userDict )
         
-        # check is login sucess
+        # check is login success
         session.headers['Host'] = 'my.ntu.edu.tw'
         session.headers['Referer'] = 'https://web2.cc.ntu.edu.tw/p/s/login2/p1.php'
         # back to attend sys. page
@@ -166,26 +255,40 @@ if __name__ == "__main__" :
         if request.status_code != 200:
             raise Exception("Login Error: please check your network connection!")
         soup = BeautifulSoup(request.text, 'html.parser')
-        # check is sigin/signout btn. exisit
+        # check is signin/signout btn. exisit
         btnDiv = soup.find("div", "jumbotron mid bc jumbotronfix")
         if not btnDiv :
-            raise Exception("Login Error?: please check your user name or password!")
+            raise Exception("Login Error: please check your user name or password!")
         btnList = btnDiv.find_all('a')
         if len(btnList) != 2 :
-            raise Exception("Login Error?: please check your user name or password!")
+            raise Exception("Login Error: please check your user name or password!")
         
         # signin / signout
         if btnList[0].get("id") == "btSign" and btnList[1].get("id") == "btSign2" :
-            viewState = soup.find("input", id="__VIEWSTATE", type="hidden").get("value")
-            viewStateGenerator = soup.find("input", id="__VIEWSTATEGENERATOR", type="hidden").get("value")
-        
+            # viewState = soup.find("input", id="__VIEWSTATE", type="hidden").get("value")
+            # viewStateGenerator = soup.find("input", id="__VIEWSTATEGENERATOR", type="hidden").get("value")
+            
             if action == "signin" :
                 messageDict = signIn(session);
             elif action == "signout" :
                 messageDict = signOut(session);
             else :
-                messageDict = {"Error": "Action Wrong"}
+                messageDict = {'t': -1, "msg": "Wrong Signing Action"}
             print(messageDict)
+            # check is signing success
+            checkDict = checkSignSuccess( messageDict )
+            
     except Exception as e :
-        print(e)
-        # raise Exception("Exception Error as "+e)
+        checkDict['success'] = False
+        checkDict['massage'] = "Exception Error : " + str(e)
+        messageDict = {'t': -1, "msg": traceback.format_exc()}
+        print("Exception Error : " + str(e))
+        print(traceback.format_exc())
+        
+    finally :
+        if not checkDict['success'] and "MAIL" in config and config["MAIL"]["SendWraningMail"].lower() == "true" :
+            try :
+                sendErrorMessageMail( checkDict['massage'], messageDict, config["MAIL"] )
+            except Exception as e_mail :
+                print("Exception Error : " + str(e_mail))
+                print(traceback.format_exc())
